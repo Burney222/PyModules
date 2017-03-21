78399ea9f6b99e8c82f1f9a500c4934a2ed16fc0
Daniel Berninghoff <daniel.berninghoff@gmail.com>
2017-03-21 13:33:40 +0100

    Changes for different matplotlib versions

--- gc_colors.py
+++ gc_colors.py
@@ -1,24 +1,27 @@
-import matplotlib.colors as colors
+import matplotlib as mpl
 import pprint
 
-gc_colors = { "gcred" : "#BE1818",
-              "gcorange" : "#FF9900",
-              "gclime" : "#9DCE09",
-              "gcgreen" : "#488F38",
-              "gcdarkgrey" : "#808080",
-              "gcgrey" : "#C0C0C0",
-              "gclightgrey" : "#E6E6E6",
-              "gcsilver" : "#EFF2F9",
-              "gcdarkblue" : "#00006E",
-              "gcblue" : "#000099",
-              "gcgreyblue" : "#1C3363",
-              "gccyan" : "#009999",
-              "gcgreycyan" : "#BBE0E3" }
+gc_colors = { u"gcred" : u"#BE1818",
+              u"gcorange" : u"#FF9900",
+              u"gclime" : u"#9DCE09",
+              u"gcgreen" : u"#488F38",
+              u"gcdarkgrey" : u"#808080",
+              u"gcgrey" : u"#C0C0C0",
+              u"gclightgrey" : u"#E6E6E6",
+              u"gcsilver" : u"#EFF2F9",
+              u"gcdarkblue" : u"#00006E",
+              u"gcblue" : u"#000099",
+              u"gcgreyblue" : u"#1C3363",
+              u"gccyan" : u"#009999",
+              u"gcgreycyan" : u"#BBE0E3" }
 
 
 #Patch matplotlib color palette
-colors.cnames.update(gc_colors)
 
+if int(mpl.__version__[0]) < 2: 
+    mpl.colors.cnames.update(gc_colors)
+else:
+    mpl.colors._colors_full_map.update(gc_colors)
 
 def show():
     #Function to plot the GC colors
