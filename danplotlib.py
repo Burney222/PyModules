8a50703c0f0de32341521bc2ed7230ab0864fc66
Daniel Berninghoff <daniel.berninghoff@cern.ch>
2017-08-25 12:03:51 +0200

    Added function "autoscale_nooutliers" which adjusts the limits automatically and removes outliers

--- danplotlib.py
+++ danplotlib.py
@@ -4,6 +4,7 @@ Module for convenient plotting using matplotlib.pyplot
 from functools import wraps, partial, update_wrapper
 import matplotlib.pyplot as plt
 import matplotlib as mpl
+import numpy as np
 
 
 #Define new plot functions which also plot minorticks
@@ -69,3 +70,77 @@ def trafo_subplots_axes(func):
     return func_wrapper
 
 subplots = trafo_subplots_axes(plt.subplots)
+
+
+
+
+
+#=================================================================
+# Add additional functionalities =================================
+#=================================================================
+
+def is_outlier(points, thresh=3.5):
+    """
+    Returns a boolean array with True if points are outliers and False
+    otherwise.
+
+    Parameters:
+    -----------
+        points : An numobservations by numdimensions array of observations
+        thresh : The modified z-score to use as a threshold. Observations with
+            a modified z-score (based on the median absolute deviation) greater
+            than this value will be classified as outliers.
+
+    Returns:
+    --------
+        mask : A numobservations-length boolean array.
+
+    References:
+    ----------
+        Boris Iglewicz and David Hoaglin (1993), "Volume 16: How to Detect and
+        Handle Outliers", The ASQC Basic References in Quality Control:
+        Statistical Techniques, Edward F. Mykytka, Ph.D., Editor.
+    """
+    if len(points.shape) == 1:
+        points = points[:,None]
+    median = np.median(points, axis=0)
+    diff = np.sum((points - median)**2, axis=-1)
+    diff = np.sqrt(diff)
+    med_abs_deviation = np.median(diff)
+
+    modified_z_score = 0.6745 * diff / med_abs_deviation
+
+    return modified_z_score > thresh
+
+#Function to adjust scaling, ignoring outliers
+def autoscale_nooutliers(axis=None, direction="y", extend_view=0.1, thresh=3.5):
+    if not axis:
+        axis=plt.gca()
+
+    if direction not in ["x", "y", "both"]:
+        raise ValueError("direction must be either 'x', 'y' or 'both'!")
+    elif direction == "both":
+        direction = ["x", "y"]
+    else:
+        direction = [ direction ]
+
+    #Loop through directions
+    for xory in direction:
+        low = np.nan
+        up = np.nan
+        #Loop through line objects of axis
+        for line in axis.lines:
+            data = line.get_xdata() if xory == "x" else line.get_ydata()
+            if not isinstance(data, list): #Ignore axvline, axhline
+                data_filtered = data[~is_outlier(data, thresh)]
+                up = np.nanmax( [np.nanmax(data_filtered), up] )
+                low = np.nanmin( [np.nanmin(data_filtered), low] )
+
+        if xory == "x":
+            axis.set_xlim(low-extend_view*(up-low), up+extend_view*(up-low))
+        else:
+            axis.set_ylim(low-extend_view*(up-low), up+extend_view*(up-low))
+
+
+#Patch matplotlib Axes
+mpl.axes.Axes.autoscale_nooutliers = autoscale_nooutliers
