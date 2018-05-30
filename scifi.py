Corrected time -> course/fine calculation

scifi.py
@@ -1,4 +1,5 @@
-# SciFi related functions
+# SciFi/MiniDAQ related functions
+import numpy as np
 
 
 def coursefine2time(course, fine):
@@ -9,5 +10,7 @@ def coursefine2time(course, fine):
 def time2coursefine(time):
     """Function to convert time (in ps) to (course, fine) value pair"""
     course = time//781.25
-    fine = (time%781.25)/48.8
+    fine = np.round((time%781.25)/48.8)
+    course += (fine//16)
+    fine = (fine%16)
     return (course, fine)